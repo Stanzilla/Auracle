27c11642fdae6c280160b3374a25d6b1d8b91aeb
taleden <taleden@3e104c00-bfb3-44b4-97f7-7ed2244fb7b9>
2009-08-01 13:59:40 +0000

    - forgot to include the builtin GlobalStrings in the actual locale files

--- trunk/locales/esES.lua
+++ trunk/locales/esES.lua
@@ -5,5 +5,25 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Auracle", "esES")
 if not L then return end
 
+L["ARENA"] = ARENA -- noun
+L["AURAS"] = AURAS -- plural noun
+L["BACKGROUND"] = EMBLEM_BACKGROUND -- noun
+L["BATTLEGROUND"] = BATTLEGROUND -- noun
+L["BORDER"] = EMBLEM_BORDER -- noun
+L["COLOR"] = COLOR -- noun
+L["DEFAULT"] = DEFAULT
+L["DELETE"] = DELETE -- verb
+L["DISPLAY"] = DISPLAY -- verb
+L["FOCUS"] = FOCUS -- i.e. the "focus" unitid
+L["FRIENDLY"] = FRIENDLY -- i.e. reacation type
+L["HOSTILE"] = HOSTILE -- i.e. reacation type
+L["ICON"] = EMBLEM_SYMBOL
+L["NAME"] = NAME -- noun
+L["NEUTRAL"] = FACTION_STANDING_LABEL4 -- i.e. reacation type
+L["NONE"] = NONE -- adjective
+L["PET"] = PET -- i.e. the "pet" unitid
+L["PLAYER"] = PLAYER -- i.e. the "player" unitid
+L["TARGET"] = TARGET -- i.e. the "target" unitid
+
 --@localization(locale="esES", format="lua_additive_table", handle-unlocalized="comment")@ 
 
